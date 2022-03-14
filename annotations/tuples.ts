737589dfd0a2b3f6fc2cce51728b288e6a6d11c2
sophiehuhuhu <sophiehu36@163.com>
2022-03-14 17:56:20 +0800

    Create tuples.ts

---
+++ annotations/tuples.ts
@@ -0,0 +1,19 @@
+const drink = {
+    color: "brown",
+    carbonated: true,
+    sugar: 40,
+};
+
+// Type alias
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ["brown", true, 40];
+const sprite: Drink = ["clear", true, 40];
+
+const carSpecs: [number, number] = [400, 3354];
+
+// object is much easier to understand
+const carStats = {
+    horsepower: 400,
+    weight: 3354,
+};
