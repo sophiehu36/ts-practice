90aef7ce2f1a737146653720e894c412cceacfe8
sophiehu36 <sophiehu36@163.com>
2022-03-18 18:36:05 +0800

    Update User.ts

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,7 +1,12 @@
+import faker from "faker";
+
 class User {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
+  constructor() {
+    this.name = faker.name.firstName()
+  }
 }
